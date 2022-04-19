385f71831d9246b2ef4baf3674e88113d1c82968
krenilpatel11 <68178999+krenilpatel11@users.noreply.github.com>
2022-04-19 11:38:41 +0530

    Updates

---
+++ precache-manifest.b3729b22eb21c4a66324b166ae914636.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "141d9bcdd1302f17e087",
+    "url": "/React_Pizza_Webpage/static/css/main.37f8a558.chunk.css"
+  },
+  {
+    "revision": "141d9bcdd1302f17e087",
+    "url": "/React_Pizza_Webpage/static/js/main.141d9bcd.chunk.js"
+  },
+  {
+    "revision": "ac94cf8ca3de808e791c",
+    "url": "/React_Pizza_Webpage/static/js/1.ac94cf8c.chunk.js"
+  },
+  {
+    "revision": "90b7ab4da84960af8b97",
+    "url": "/React_Pizza_Webpage/static/js/runtime~main.90b7ab4d.js"
+  },
+  {
+    "revision": "d85cec468342f3dcee087c02bf6dd430",
+    "url": "/React_Pizza_Webpage/static/media/pizzaLogo.d85cec46.png"
+  },
+  {
+    "revision": "4aa5292cac69561b68b10b2db0f3f6c1",
+    "url": "/React_Pizza_Webpage/static/media/pizza.4aa5292c.jpeg"
+  },
+  {
+    "revision": "08e289f85265b3d2b3984db3d424b705",
+    "url": "/React_Pizza_Webpage/static/media/pepperoni.08e289f8.jpg"
+  },
+  {
+    "revision": "434161f0b4c193f9ce5e1061f26dbf40",
+    "url": "/React_Pizza_Webpage/static/media/margherita.434161f0.jpg"
+  },
+  {
+    "revision": "05f53a063fe35296c578593c523ab1a1",
+    "url": "/React_Pizza_Webpage/static/media/pedrotechspecial.05f53a06.jpg"
+  },
+  {
+    "revision": "1672e7ad52375097a95ec88034fbc0cb",
+    "url": "/React_Pizza_Webpage/static/media/vegan.1672e7ad.jpg"
+  },
+  {
+    "revision": "44f59bed20e652447ce9f2f2c9340c65",
+    "url": "/React_Pizza_Webpage/static/media/pineapple.44f59bed.jpg"
+  },
+  {
+    "revision": "7f204ab1930eb1878e1a3326ec315244",
+    "url": "/React_Pizza_Webpage/static/media/expensive.7f204ab1.jpg"
+  },
+  {
+    "revision": "67f4f9031213dbbb6c7aaf1795c57ef8",
+    "url": "/React_Pizza_Webpage/static/media/multiplePizzas.67f4f903.jpeg"
+  },
+  {
+    "revision": "e29e930c1870f3fcc15edb3a8c3ade19",
+    "url": "/React_Pizza_Webpage/static/media/pizzaLeft.e29e930c.jpg"
+  },
+  {
+    "revision": "52dac1fb749867d4742f4d3d96ef6b57",
+    "url": "/React_Pizza_Webpage/index.html"
+  }
+];
