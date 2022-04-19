77ccfc70f3e452a5b5064d69e6a03a83d372de43
krenilpatel11 <68178999+krenilpatel11@users.noreply.github.com>
2022-04-19 11:20:08 +0530

    Add files via upload

---
+++ src/pages/Contact.js
@@ -0,0 +1,30 @@
+import React from "react";
+import PizzaLeft from "../assets/pizzaLeft.jpg"
+import "../styles/Contact.css"
+
+function Contact() {
+  return (
+    <div className="contact">
+      <div
+        className="leftSide"
+        style={{
+          backgroundImage: `url(${PizzaLeft})`,
+        }}
+      ></div>
+      <div className="rightSide">
+          <h1>Contact Us</h1>
+          <form id="contact-form" method="POST">
+              <label htmlfor="name">Full Name</label>
+              <input name="name" placeholder="Enter full name..." type="text"/>
+              <label htmlfor="email">Email</label>
+              <input name="email" placeholder="Enter email..." type="email"/>
+              <label htmlfor="message">Message</label>
+              <textarea row="6" placeholder="Enter message..." name="message" required ></textarea>
+              <button type="submit">Send Message</button>
+          </form>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
